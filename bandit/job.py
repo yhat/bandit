7e68cf6ef4ab981677fa496437daf8a395f8bbe8
Greg Lamp <lamp.greg@gmail.com>
2016-12-21 17:07:04 -0500

    docstrings

--- bandit/job.py
+++ bandit/job.py
@@ -5,6 +5,21 @@ def metadata(json_blob):
     _write_metadata(json_blob)
 
 def add_metadata_key(key, value):
+    """
+    Add key value pair to your metdata.
+
+    Parameters
+    ==========
+    key: str
+        name of the value you'd like to save
+    value: object
+        value of what you'd like to save
+
+    Examples
+    ========
+    >>> add_metadata_key("r2", 0.82)
+    >>> add_metadata_key("foo", "bar")
+    """
     metadata = _get_metadata()
     metadata[key] = value
     _write_metadata(json.dumps(metadata))
@@ -40,6 +55,9 @@ function and passing it stringified HTML or a plaintext string.""".replace("\n",
 _DEFAULT_BODY += "\n\nCheers!\n~Team Bandit"
 
 class Email(object):
+    """
+    Use the Email objects to programatically send e-mail alerts with Bandit.
+    """
     def __init__(self, subject="", body="", attachments=[], write_json=True):
         self._subject = subject if subject else "Bandit Job"
         self._body = body if body else _DEFAULT_BODY
@@ -47,6 +65,7 @@ class Email(object):
         self.write_json = write_json
 
     def _to_dict(self):
+        "private method for dictifying an Email"
         return {
             "subject": self._subject,
             "body": self._body,
@@ -54,6 +73,10 @@ class Email(object):
         }
 
     def _write(self):
+        """
+        Emails get written to a JSON file by default. This is then picked up by
+        the bandit job runner and converted into a *real* email.
+        """
         if self.write_json==False:
             return
 
@@ -61,6 +84,17 @@ class Email(object):
             json.dump(self._to_dict(), f)
 
     def __str__(self):
+        """
+        Default stringified message looks like this:
+
+        > Bandit Job
+        Your job has completed. This is the default message. You can customize
+        this message by calling the `bandit.job.body(html or string)` function
+        and passing it stringified HTML or a plaintext string.
+
+        Cheers!
+        ~Team Bandit
+        """
         msg = [
             "="*80,
             "> %s" % self._subject,
@@ -70,14 +104,39 @@ class Email(object):
         return "\n".join(msg)
 
     def subject(self, string):
+        """
+        Add a subject to your email
+
+        Parameters
+        ==========
+        string: str
+            the subject line of your email
+        """
         self._subject = string
         self._write()
 
     def body(self, html_or_string):
+        """
+        Add a body to your email
+
+        Parameters
+        ==========
+        html_or_string: str
+            the body of your email. this can be either HTML or plaintext
+        """
         self._body = html_or_string
         self._write()
 
     def attachment(self, attachment):
+        """
+        Add an attachment to your email
+
+        Parameters
+        ==========
+        attachment: instance of StringIO
+            attachment for your email. this needs to be a StringIO object. you can
+            format most file types as a StringIO filetype (for example a png)
+        """
         self._attachments.append(attachment)
 
 # email = Email(write_json=False)
