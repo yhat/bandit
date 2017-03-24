28a143bf847ef4291fa7b68307f9753fe0048438
Greg Lamp <lamp.greg@gmail.com>
2017-03-24 11:28:12 -0400

    docstring

--- bandit/bandit.py
+++ bandit/bandit.py
@@ -219,9 +219,9 @@ class Bandit(object):
         ========
         >>> from ggplot import mtcars
         >>> bandit = Bandit()
-        >>> print bandit.make_dashboard(table=mtcars.to_html(classes="table"))
-        >>> print bandit.make_dashboard(table=mtcars)
-        >>> bandit.make_dashboard(template_name='many-tables', tables=[mtcars.head().to_html(classes='table'), mtcars.tail().to_html(classes='table')])
+        >>> print bandit.make_dashboard("my dashboard", table=mtcars.to_html(classes="table"))
+        >>> print bandit.make_dashboard("my dashboard", table=mtcars)
+        >>> bandit.make_dashboard("my dashboard", template_name='many-tables', tables=[mtcars.head().to_html(classes='table'), mtcars.tail().to_html(classes='table')])
         """
         variables = {}
         for key, value in kwargs.items():
